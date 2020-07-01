f701255ddf927a3f5db05769f8f411c8164e6fb5
Rafael Zurita <rafa@fi.uncoma.edu.ar>
2020-07-01 09:12:22 -0300

    web

--- www/build_web.sh
+++ www/build_web.sh
@@ -1,6 +1,6 @@
 
 markdown_py -f index.html $1
-echo '<br><br><hr> last edit: '$(date)'- Rafael Ignacio Zurita <rafa@fi.uncoma.edu.ar> <br>' > /tmp/deleteme2
+echo '<br><br><hr><small> last edit: '$(date)' - <i>Rafael Ignacio Zurita (rafa at fi.uncoma.edu.ar)</i></small> <br><br>' > /tmp/deleteme2
 cat w1.html index.html /tmp/deleteme2 w2.html | grep -v "visit the Xinu" > /tmp/deleteme.html
 rm index.html
 rm /tmp/deleteme2
