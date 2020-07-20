e9604a8914f43552a2cb09214b561a93ea0ca269
Rafael Zurita <rafaelignacio.zurita@gmail.com>
2020-07-20 18:28:50 -0300

    example0

--- apps/example0/main.c
+++ apps/example0/main.c
@@ -7,19 +7,15 @@
 
 process example0(int nargs, char *args[])
 {
-	printf("\n  Starting %s process\n", proctab[currpid].prname);
 	printf("  nargs = %d\n", nargs);
 	printf("  args[0]: %d\n", args[0]);
 	printf("  args[1]: %s\n", args[1]);
-	printf("  %s process has completed.\n", proctab[currpid].prname);
 
 	return OK;
 }
 
 process	main(void)
 {
-	printf("\nStarting %s process\n", proctab[currpid].prname);
-
 	/* We create() a new process. Arguments:
 	 *         process code: example0()
 	 *         stack size: 128 bytes
@@ -35,7 +31,7 @@ process	main(void)
 	/* Wait for main to exit */
 
 	receive();
-	printf("\n%s process has completed.\n", proctab[currpid].prname);
+	printf("\n %s process has completed.\n", proctab[currpid].prname);
 
 	return OK;
 }
