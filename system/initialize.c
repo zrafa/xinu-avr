9be6b4de34343acb27eeb969042e0cc70280de95
Rafael Zurita <rafaelignacio.zurita@gmail.com>
2020-07-22 16:50:22 -0300

    initialize

--- system/initialize.c
+++ system/initialize.c
@@ -16,8 +16,7 @@
 extern	void main(void);	/* Main is the first process created	*/
 static	void sysinit(); 	/* Internal system initialization	*/
 extern	void meminit(void);	/* Initializes the free memory list	*/
-//local	process startup(void);	/* Process to finish startup tasks	*/
-void startup(int INIT, struct procent *p);
+void startup(int, struct procent *); /* Process to finish startup tasks	*/
 
 /* Declarations of major kernel variables */
 
@@ -50,30 +49,11 @@ pid32	currpid;		/* ID of currently executing process	*/
 
 void nullprocess(void) {
 
+	// resume(create((void *)main, INITSTK, INITPRIO, "Main Process", 0, NULL));
 	resume(create((void *)main, 440, INITPRIO, "main", 0, NULL));
-
+	
 	for(;;);
 }
-
-/* avr specific */
-#define GET_FAR_ADDRESS(var)                          \
-({                                                    \
-    uint32_t tmp;                                     \
-                                                      \
-    __asm__ __volatile__(                             \
-                                                      \
-            "ldi    %A0, lo8(%1)"           "\n\t"    \
-            "ldi    %B0, hi8(%1)"           "\n\t"    \
-            "ldi    %C0, hh8(%1)"           "\n\t"    \
-            "clr    %D0"                    "\n\t"    \
-        :                                             \
-            "=d" (tmp)                                \
-        :                                             \
-            "p"  (&(var))                             \
-    );                                                \
-    tmp;                                              \
-})
-
 
 void	nulluser()
 {	
@@ -84,15 +64,8 @@ void	nulluser()
 
 	sysinit();
 
-	/* avr specific */
-	extern uint32 __bss_end, __data_start;
-	uint32 ptr_bss_end;
-	ptr_bss_end = GET_FAR_ADDRESS(__bss_end);  //get the pointer
-
-	uint32 ptr_data_start;
-	ptr_data_start = GET_FAR_ADDRESS(__data_start);  //get the pointer
-
 	/* Output Xinu memory layout */
+
 	free_mem = 0;
 	for (memptr = memlist.mnext; memptr != NULL;
 						memptr = memptr->mnext) {
@@ -100,60 +73,37 @@ void	nulluser()
 	}
 	kprintf("\nFreeMEM:%d (bytes)\n\n", free_mem);
 
-
-	/* Become the Null process (i.e., guarantee that the CPU has	*/
-	/*  something to run when no other process is ready to execute)	*/
-
-
 	/* Initialize the Null process entry */	
+
 	int pid = create((void *)nullprocess, INITSTK, 10, "nullp", 0, NULL);
 	struct procent * prptr = &proctab[pid];
 	prptr->prstate = PR_CURR;
-
+	
 	/* Enable interrupts */
 
 	enable();
 	
-	/* create main without startup because:
- 	 *  - startup is not needed so far 
-	 *  - it fragments the free memory in two
-	 */
-	//resume(create((void *)main, 440, INITPRIO, "main", 0, NULL));
-
 	/* Start of nullprocess */
-	startup(0, prptr);
 
-
-        /* Become the Null process (i.e., guarantee that the CPU has    */
-        /*  something to run when no other process is ready to execute) */
+	resume(create((void *)main, 440, INITPRIO, "main", 0, NULL));
+	//startup(0, prptr);
 
 	for(;;);
 
 }
 
-/*------------------------------------------------------------------------
- *
- * startup  -  Finish startup takss that cannot be run from the Null
- *		  process and then create and resumethe main process
- *
- *------------------------------------------------------------------------
- */
-void startup(int INIT, struct procent *p)
-// local process	startup(void)
-{
-	/* Create a process to execute function main() */
-
-	//resume(create((void *)main, 440, INITPRIO, "main", 0, NULL));
-
+/* Startup does a system call, the processor switches to handler 
+ * mode and prepares for executing the null process (see syscall.c) 
+ * This is also where a kernel mode to user mode switch can
+ * take place */
+void startup(int INIT, struct procent *p) {
+	
 	nullprocess();
+
+	/* Should not be here, panic */
 	avr_kprintf(m6);
 	panic("");
-
-	/* Startup process exits at this point */
-
-	return OK;
 }
-
 
 
 /*------------------------------------------------------------------------
@@ -183,7 +133,8 @@ static	void	sysinit()
 	/* Initialize system variables */
 
 	/* Count the Null process as the first process in the system */
-	prcount = 1;
+	prcount = 0;
+//	prcount = 1;
 
 	/* Scheduling is not currently blocked */
 
@@ -199,26 +150,6 @@ static	void	sysinit()
 		prptr->prprio = 0;
 	}
 
-	/* Initialize the Null process entry */	
-
-/*
-	prptr = &proctab[NULLPROC];
-	prptr->prstate = PR_CURR;
-	prptr->prprio = 1;
-	prptr->prname[0] = 'n';
-	prptr->prname[1] = 'u';
-	prptr->prname[2] = 'l';
-	prptr->prname[3] = 'l';
-	prptr->prname[4] = 'p';
-	prptr->prname[5] = 0;
-*/
-	// strncpy(prptr->prname, "prnull", 7);
-/*
-	prptr->prstkbase = getstk(NULLSTK);
-	prptr->prstklen = NULLSTK;
-	prptr->prstkptr = 0;
-	currpid = NULLPROC;
-*/
 
 	/* Initialize semaphores */
 
@@ -237,15 +168,14 @@ static	void	sysinit()
 	/* Create a ready list for processes */
 
 	readylist = newqueue();
-
-	/* Initialize the real time clock */
-
-	clkinit();
-
 	
 	for (i = 0; i < NDEVS; i++) {
 		init(i);
 	}
+
+	/* Initialize the real time clock */
+
+	clkinit();
 
 	return;
 }
