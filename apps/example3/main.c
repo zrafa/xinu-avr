ddbfc0fa6a38e569793ed6d57ce0a12fbf5c987e
rzurita <rzurita@debian-8-amd64.fi.uncoma.edu.ar>
2022-03-29 16:20:15 -0300

    book

--- apps/example3/main.c
+++ apps/example3/main.c
@@ -2,7 +2,7 @@
 
 #include <xinu.h>
 
-void	sndch(int nargs, char *args[]);
+void	sndch(char);
 
 /*------------------------------------------------------------------------
  * main  --  example of 2 processes executing the same code concurrently
@@ -10,18 +10,17 @@ void	sndch(int nargs, char *args[]);
  */
 void	main(void)
 {
-	resume( create(sndch, 128, 20, "send A", 1, 'A') );
-	resume( create(sndch, 128, 20, "send B", 1, 'B') );
+	resume( create(sndch, 1024, 20, "send A", 1, 'A') );
+	resume( create(sndch, 1024, 20, "send B", 1, 'B') );
 }
 
 /*------------------------------------------------------------------------
  * sndch  --  output a character on a serial device indefinitely
  *------------------------------------------------------------------------
  */
-void	sndch(int nargs, char *args[])
+void	sndch(char ch)
 
 {
-	char ch = args[0]; /* character to emit continuously */
 
 	while ( 1 )
 		putc(CONSOLE, ch);
